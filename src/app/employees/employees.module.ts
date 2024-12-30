4b3abf28ff585e35d2184d30a06d17a08b846106
kholoud001 <kholoud.sanak@gmail.com>
2024-12-30 09:53:38 +0100

    EM-6 component creation

--- src/app/employees/employees.module.ts
+++ src/app/employees/employees.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeesRoutingModule } from './employees-routing.module';
+import { EmployeeListComponent} from './employee-list/employee-list.component';
+import { EmployeeCardComponent} from './employee-card/employee-card.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EmployeeListComponent,
+    EmployeeCardComponent
+  ],
   imports: [
     CommonModule,
     EmployeesRoutingModule
