013b0a5d27e9f0a6e4e7e967b569338ab8b5331d
kholoud001 <kholoud.sanak@gmail.com>
2024-12-30 14:32:39 +0100

    EM-16 pipe date format

--- src/app/employees/employees.module.ts
+++ src/app/employees/employees.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, DatePipe} from '@angular/common';
 
 import { EmployeesRoutingModule } from './employees-routing.module';
 import { EmployeeListComponent} from './employee-list/employee-list.component';
 import { EmployeeCardComponent} from './employee-card/employee-card.component';
 import {EmployeeFormComponent} from './employee-form/employee-form.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {DateFormatPipe} from '../date-format.pipe';
 
 
 @NgModule({
@@ -13,11 +14,14 @@ import {ReactiveFormsModule} from '@angular/forms';
     EmployeeListComponent,
     EmployeeCardComponent,
     EmployeeFormComponent,
+    DateFormatPipe,
   ],
   imports: [
     CommonModule,
     EmployeesRoutingModule,
     ReactiveFormsModule
-  ]
+  ],
+  providers: [DatePipe],
+
 })
 export class EmployeesModule { }
