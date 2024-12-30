a64dbdd9554f834907d705af5c48097a804299f7
kholoud001 <kholoud.sanak@gmail.com>
2024-12-30 10:25:18 +0100

    EM-7 emplyee forms

--- src/app/employees/employees.module.ts
+++ src/app/employees/employees.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { EmployeesRoutingModule } from './employees-routing.module';
 import { EmployeeListComponent} from './employee-list/employee-list.component';
 import { EmployeeCardComponent} from './employee-card/employee-card.component';
+import {EmployeeFormComponent} from './employee-form/employee-form.component';
 
 
 @NgModule({
   declarations: [
     EmployeeListComponent,
-    EmployeeCardComponent
+    EmployeeCardComponent,
+    EmployeeFormComponent,
   ],
   imports: [
     CommonModule,
