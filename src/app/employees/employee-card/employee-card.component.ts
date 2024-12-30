a6416c4f0215b7cd3e6d9b5944baa3c565543182
kholoud001 <kholoud.sanak@gmail.com>
2024-12-30 10:12:56 +0100

    EM-3 CRUD employees & LocalStorage

--- src/app/employees/employee-card/employee-card.component.ts
+++ src/app/employees/employee-card/employee-card.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {EmployeeService} from '../employee.service';
 
 @Component({
   selector: 'app-employee-card',
-  imports: [],
+  providers: [EmployeeService],
   templateUrl: './employee-card.component.html',
   styleUrl: './employee-card.component.css'
 })
 export class EmployeeCardComponent {
+  constructor(private employeeService:EmployeeService) {
+  }
+
 
 }
--- src/app/employees/employee.service.ts
+++ src/app/employees/employee.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
+
 export class EmployeeService {
 
-  constructor() { }
+  //constructor() { }
+  private employees=JSON.parse(localStorage.getItem('employees')||'[]');
+
+  getEmployess(){
+    return this.employees;
+  }
+
+  addEmployee(employee:any){
+    this.employees.push(employee);
+    this.saveToLocalStorage();
+  }
+
+  updateEmployee(index: number, employee: any) {
+    this.employees[index] = employee;
+    this.saveToLocalStorage();
+  }
+
+  deleteEmployee(index: number) {
+    this.employees.splice(index, 1);
+    this.saveToLocalStorage();
+  }
+
+  private saveToLocalStorage(){
+    localStorage.setItem('employees',JSON.stringify(this.employees));
+  }
+
+
 }
