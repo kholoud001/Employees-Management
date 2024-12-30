05b0d3608d633bfa879ce1a3104be38dc0ed1396
kholoud001 <kholoud.sanak@gmail.com>
2024-12-30 10:54:29 +0100

    lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employees', loadChildren: () => import('./employees/employees.module').then(m => m.EmployeesModule) },
+  { path: '', redirectTo: '/employees', pathMatch: 'full' }, // Redirige vers la liste des employés par défaut
+  { path: '**', redirectTo: '/employees' } // Gestion des routes inexistantes
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
